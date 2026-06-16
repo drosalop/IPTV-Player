1a22fbb15e898e9dc86a1182e43790d927f5cb72
drosalop <davorsevilla@hotmail.com>
2026-06-16 16:44:12 +0200

    feat: implement centralized state manager in store.js

--- js/store.js
+++ js/store.js
@@ -10,9 +10,23 @@ const Store = (() => {
     groupCountsCache: null
   };
 
+  let listeners = {};
+
   return {
     get: (key) => state[key],
-    set: (key, val) => { state[key] = val; },
+    set: (key, val) => { 
+      state[key] = val; 
+      if (listeners[key]) {
+        listeners[key].forEach(cb => { try { cb(val); } catch(e) { console.error(e); } });
+      }
+    },
+    subscribe: (key, cb) => {
+      if (!listeners[key]) listeners[key] = [];
+      listeners[key].push(cb);
+      return () => {
+        listeners[key] = listeners[key].filter(item => item !== cb);
+      };
+    },
     getAll: () => state
   };
 })();
